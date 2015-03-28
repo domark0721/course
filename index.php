8501221f32e9c491d81795653e90f2b8050c6e4d
Mark <domark0721@gmail.com>
2015-03-28 18:17:46 +0800

    headbar v3 finish

--- index.php
+++ index.php
@@ -10,16 +10,25 @@
 	</head>
 	<body>
 		<?php require("header.php") ?>
-		<div id="section-search-wrap">
-			<div id="searchControl">
-				<form id="searchForm"method="GET" action="">
-					<input id="inputCourse" type="text" placeholder="您想要學習什麼課程？">
-					<button class="searchbtn">搜尋</button>
-				</form>
+		<div id="section-search">
+		       <div class="content-wrap">
+				<div id="searchControl">
+					<form id="searchForm"method="GET" action="">
+						<input id="inputCourse" type="text" placeholder="您想要學習什麼課程？">
+						<button class="searchbtn">搜尋</button>
+					</form>
+				</div>
 			</div>
 		</div>
-		<div id="section-hotcourse-wrap">
-			
+		<div id="section-hotcourse">
+			<div class="content-wrap">
+				<div class="indexLabel">熱門課程</div>
+					<div id="classlist-container">
+						<ul>
+							<li></li>
+						</ul>
+					</div>
+			</div>
 		</div>
 	</body>
 </html>
